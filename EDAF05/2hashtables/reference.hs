f4434ddbfd26c6abb827f3dc5afd440f867ba9d5
Leo Haker <haker338@gmail.com>
2026-04-12 22:55:41 +0200

    funkar inte än, men bra grund

--- EDAF05/2hashtables/reference.hs
+++ EDAF05/2hashtables/reference.hs
@@ -9,21 +9,35 @@ emptyMap = Map.empty
 
 main = do
     counter <- newIORef 0
-    let emptyMap = Map.empty
-    loop counter emptyMap
+    hashMap <- loop counter Map.empty
+    let
+        (count, word) = maximum (zip (Map.elems hashMap)  (Map.keys hashMap))
+        possibleWords = Map.keys $ Map.filterWithKey(\k v -> v == count &&  k < word) hashMap
+        word' = if null possibleWords then word else minimum possibleWords
+    print(word, count)
 
-
-loop :: IORef Int -> Map.Map String Int-> IO ()
+loop :: IORef Int -> Map.Map String Int-> IO (Map.Map String Int)
 loop counter hashMap = do
     eof <- isEOF -- kollar om stdIn är slut
     if eof
-        then return ()
+        then return hashMap
         else do
             i <- readIORef counter       -- läsa
             line <- getLine
+            modifyIORef' counter (+1) 
             let word = strip line
-            -- gör något
-            loop counter hashMap
+                isPresent = Map.member word hashMap
+                removeIt = (i `mod` 16) == 0
+            if isPresent
+                then if removeIt
+                        then loop counter (Map.delete word hashMap)
+                        else loop counter (Map.update addOne word hashMap)
+                else if not removeIt 
+                        then loop counter (Map.insert word 1 hashMap)
+                        else loop counter hashMap
 
 strip :: String -> String
 strip string = reverse $ dropWhile(== ' ') $ reverse $ dropWhile(== ' ') string
+
+addOne :: Num a => a -> Maybe a
+addOne x = Just(x + 1)
