983f87858255c09c66e976e38f5afa8fae5b02f8
KalleSkog <kalle.skog@hotmail.com>
2026-04-21 14:27:08 +0200

    hejsan2

--- EDAF05/2hashtables/hash.py
+++ EDAF05/2hashtables/hash.py
@@ -11,7 +11,47 @@ def main():
     print("hejsan")
 
 class SeparateChaining:
+    def __init__(self):
+        self.hashtable = [[]]
+        self.size = 1
     
+    def insert(self, n: Node): 
+        hashKey = string_to_hash(n.key)
+        hash = self.size % hashKey
+        self.hashtable[hash].append(Node)
+        self.rearrange()
+
+    def private_insert(self, n: Node) :
+        hash = self.size % n.key
+        self.hashtable[hash].append(Node)
+
+    def alpha(self): 
+        n = 0
+        m = self.size
+        for a in self.hashtable:
+            n += len(a)
+        return n/m
+
+    def rearrange(self): 
+        if self.alpha < 1:
+            self.size += 1
+            oldtable = self.hashtable
+            self.hashtable = [[] for _ in range(self.size)]
+            for a in oldtable:
+                for b in a:
+                    self.insert(b)
+        else:
+            return
+        
+
+class Node: 
+    def __init__(self, value):
+        self.key = string_to_hash(value)
+        self.value = value
+        
+
+a = [Node]
+        
 
 
 if __name__ == "__main__":
