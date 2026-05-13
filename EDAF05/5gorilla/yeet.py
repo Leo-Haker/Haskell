1bdfde0d50d718f5638d7ccd5df876bb3a64f01d
Kalle Skog <ka2300sk-s@student.lu.se>
2026-05-13 18:16:13 +0200

    inte helt klar men nära

--- EDAF05/5gorilla/yeet.py
+++ EDAF05/5gorilla/yeet.py
@@ -1,4 +1,5 @@
 import sys
+from typing import List
 
 characters = sys.stdin.buffer.readline().decode().strip().replace(" ", "")
 
@@ -14,14 +15,97 @@ for n in range(length):
     idx += length
     cost_pair.append(row)
 
-number_quiries = data[idx]; idx += 1
+number_queries = int(data[idx]); idx += 1
 
-list_quiries = []
+list_queries = []
 
-for n in number_quiries:
-    list_quiries.append((data[idx].decode(), data[idx+1].decode()))
+for n in range(number_queries):
+    list_queries.append((data[idx].decode(), data[idx+1].decode()))
     idx += 2
 
-print(characters)
-print(cost_pair)
-print(list_quiries)
+#def doing_stuff(cost_pair: List, characters: str, query: tuple[str, str]) -> str:
+#    charindex = {c: i for i, c in enumerate(characters)}
+#    res = ""
+#    s1 = query[0]
+#    s2 = query[1]
+#    m = len(s1)
+#    n = len(s2)
+#
+#    trace = [[""]*(n+1) for _ in range(m+1)]
+#    dp = [[0] * (n+1) for _ in range(m+1)]
+#
+#
+#
+#    for i in range(m+1):
+#        trace[i][0] = "*"
+#        dp[i][0] = i*-4
+#
+#    for j in range(n+1):
+#        trace[0][j] = "*"
+#        dp[0][j] = j*-4
+#
+#    for i in range (1, m+1):
+#        for j in range(1, n+1):
+#            diag = dp[i-1][j-1]+ cost_pair[charindex[s1[i-1]]][charindex[s2[j-1]]]
+#            up   = dp[i-1][j] - 4
+#            left = dp[i][j-1] - 4
+#
+#            best = max(diag,up,left)
+#
+#            dp[i][j] = best
+#
+#            if best == diag:
+#                trace[i]
+#
+#    return dp[m][n]
+#
+def doing_stuff(cost_pair: List, characters: str, query: tuple[str, str]) -> str:
+    charindex = {c: i for i, c in enumerate(characters)}
+    res = ""
+    s1 = query[0]
+    s2 = query[1]
+    m = len(s1)
+    n = len(s2)
+
+    dp = [
+        [[0, ["", ""]] for _ in range(n+1)]
+        for _ in range(m+1)
+    ]
+
+
+    for i in range(m+1):
+        dp[i][0][0] = i*-4
+        dp[i][0][1] = ("*","*")
+
+    for j in range(n+1):
+        dp[0][j][0] = j*-4
+        dp[0][j][1] = ("*","*")
+
+    for i in range (1, m+1):
+        for j in range(1, n+1):
+            diag = dp[i-1][j-1][0]+ cost_pair[charindex[s1[i-1]]][charindex[s2[j-1]]]
+            up   = dp[i-1][j][0] - 4
+            left = dp[i][j-1][0] - 4
+
+            best = max(diag,up,left)
+
+            dp[i][j][0] = best
+
+            if best == diag:
+                dp[i][j][1][0] = dp[i-1][j-1][1][0] + s1[i-1]
+                dp[i][j][1][1] = dp[i-1][j-1][1][1] + s2[j-1]
+
+            elif best == up:
+                dp[i][j][1][0] = dp[i-1][j][1][0] + s1[i-1]
+                dp[i][j][1][1] = dp[i-1][j][1][1] + s2[j]
+
+            elif best == left:
+                dp[i][j][1][0] = dp[i][j-1][1][0] + s1[i]
+                dp[i][j][1][1] = dp[i][j-1][1][1] + s2[j-1]
+
+    return dp[m][n][1][0] + "   " + dp[m][n][1][1]
+
+for p in list_queries:
+    print(doing_stuff(cost_pair, characters, p))
+
+    
