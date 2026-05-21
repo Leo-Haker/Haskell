eae235449af7818d14c5f5caa94903ce684a58ba
Leo Haker <haker338@gmail.com>
2026-05-21 14:01:52 +0200

    yup

--- EDAF05/6railwayplanning/network_flow.py
+++ EDAF05/6railwayplanning/network_flow.py
@@ -13,13 +13,15 @@ P_routes = data[idx]; idx += 1
 
 # capacaty of network >= C_students
 
-graph = {} 
+graph = [[0] * N_nodes for _ in range(N_nodes)]
 remove_route = []
 
+
 for i in range(M_edges):
-    p = (u,v,c) = data[idx], data[idx+1], data[idx+1]
-    graph[i] = p
-    idx += 3
+    u = data[idx]; idx += 1
+    v = data[idx]; idx += 1
+    c = data[idx]; idx += 1
+    graph[u][v] = c
 
 for i in range(P_routes):
     p = data[idx];idx += 1
@@ -27,21 +29,21 @@ for i in range(P_routes):
 
 
 ## enkel schematisk grund
-def BFS (G,s,t):
+def BFS (G:dict ,s,t, parent:list):
     queue = deque()
     queue.append(s)
-    visited = bytearray(len(G))
+    visited = bytearray(N_nodes)
     visited[s] = 1
 
     while queue:
-        v = queue.popleft()
+        u = queue.popleft()
 
-        for w in neighbor(v):
-            if not visited(w):
-                visited(w) = 1 
-                queue.add(w)
-                if w == t:
-                    print("found path s-t")
-                    return
-    return
+        for v in range(len(G)):
+            if not visited[v] and G[u][v] > 0:
+                visited[v] = 1 
+                queue.append(v)
+                parent[v] = u
+                if v == t:
+                    return True
+    return False
                 
