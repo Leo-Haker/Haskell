b86e7d522317670b2d9a9c86674cad658ef8b81c
Leo Haker <haker338@gmail.com>
2026-05-22 11:07:01 +0200

    rg funktion

--- EDAF05/6railwayplanning/network_flow.py
+++ EDAF05/6railwayplanning/network_flow.py
@@ -14,6 +14,7 @@ P_routes = data[idx]; idx += 1
 # capacaty of network >= C_students
 
 graph = [[0] * N_nodes for _ in range(N_nodes)]
+flow = [[0] * N_nodes for _ in range(N_nodes)]
 remove_route = []
 
 
@@ -47,16 +48,32 @@ def BFS (G:list[list] ,s,t, parent:list):
                     return True
     return False
 
-def Residual_Graph(Graph:list[list]) -> list[list]:
-    rg = [[0] * N_nodes in range(N_nodes)]
-    for i in range(N_nodes):
-        for j in range(N_nodes):
-            if Graph[i][j] == 0: rg[i][j] = 0
-            else:
-                rg[i][j] = forward_edge() - Graph[i][j]
-    return rg
+def Update_Residual_Graph(rg:list[list] ) -> list[list]:
+    for u in range(N_nodes):
+        for v in range(N_nodes):
+            a = get_capacity(u,v)
+            b = get_flow(u,v)
+            if a > b:
+                 rg[u][v]= a - b
+                 rg[v][u] = b
+            elif a == b:
+                rg[u][v] = 0
+                rg[v][u] = b
+
+def get_capacity(u, v) -> int:
+    return graph[u][v]
+
+def get_flow(u, v) -> int:
+    return flow[u][v]
+
+def update_flow(u,v, c):
+    flow[u][v] = c
                 
 def Ford_Fulkerson():
-    while BFS:
+    graph:list[list]
+    rg = graph.copy()
+    rg:list[list]
+    flow:list[list]
+    while BFS: 
         "do stuff"
     return
